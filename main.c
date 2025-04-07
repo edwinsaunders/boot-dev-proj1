5b2bb910e91b5d3d5273f4a1465fdbd3fb32887d
Edwin Saunders <edwinsaunders36@gmail.com>
2025-04-07 18:39:26 -0400

    first commit:  letter cycling with by pressing 'a'(increment) and 'b'(decrement)

---
+++ main.c
@@ -0,0 +1,102 @@
+// GOAL:  a text-based pong clone
+
+// ball movement (ball is letter 'o')
+// paddle movement / control
+
+
+// CONSTANTS
+
+
+
+#include <ncurses.h>
+#include <unistd.h>
+
+typedef struct GameState {
+	int lpadpos;
+	int rpadpos;
+	int ballpos;
+	bool game;
+} gamestate_t;
+
+
+// update - handle game logic
+//	take input from user, update game state variables
+//	game state variables:  lpaddlepos, rpaddlepos, 
+//		ballpos, gamestate
+
+void update(gamestate_t *obj) {
+	int ch = getch(); //get input
+
+	if (ch == 'a') {
+		obj->lpadpos++;
+	}
+	else if (ch == 'b') {
+		obj->lpadpos--;
+	}
+	// if (ch == ERR) {
+    // 	return;
+	// }
+	//obj->lpadpos = 97;
+	//addch(obj->lpadpos);
+}
+
+
+// draw - output to screen
+
+int main(int argc, char **argv) {
+
+	initscr(); // Initialize ncurses
+	cbreak();
+	nodelay(stdscr, TRUE);
+
+	gamestate_t thisgame = {.lpadpos = 0, .rpadpos = 0, .ballpos = 0, .game = 1};
+
+	int timer = 10;
+	int newX = 0;
+	int newY = 0;
+	
+	while(timer) {
+		
+
+		//thisgame.lpadpos = 65;
+		
+		// addch(thisgame.lpadpos);
+		move(newX, newY);
+		// addch('o');
+		// refresh();
+		
+		// usleep(500000);
+
+		
+
+		// printw("%d\n", LINES);
+		// printw("%d", COLS);
+
+		update(&thisgame);
+		
+		addch(thisgame.lpadpos);
+		// refresh();
+		// usleep(500000);
+
+
+		//print 'o' in different places with an impulse 
+		//at 45 degrees or so
+		
+		// move (20, 20);
+		// refresh();
+	    // usleep(1000000);
+
+	    // refresh(); // Update the screen
+
+	    //move(3, 3);
+	    //int x = getch(); // Wait for user input
+	    //printw("%d", x);
+	    //if (x == 'q') {
+	    //	endwin(); // End ncurses mode
+	    //	return 0;
+	    //}
+	    // timer--;
+	}
+	endwin();
+	return 0;
+}
